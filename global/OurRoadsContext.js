ea9fa9ff2eca3301536f98611cba337c9db686f8
zidanegimiga <zidanegimigatemp@gmail.com>
2023-09-25 16:54:39 +0300

    code formatting

--- global/OurRoadsContext.js
+++ global/OurRoadsContext.js
@@ -1,21 +1,16 @@
-import { useContext, createContext, useState } from "react";
+import { createContext, useState } from "react";
 
 const GameContext = createContext();
 
-const GameProvider = ({children}) =>{
-  const [score, setScore] = useState(0)
-  const [totalScore, setTotalScore] = useState(0);
+const GameProvider = ({ children }) => {
+  const [score, setScore] = useState(0);
   const [soundEnabled, setSoundEnabled] = useState(true);
-  const calculateScore = () =>{
-    const val = score/25 * 100;
-    setTotalScore(val)
-  }  
 
-  return(
-    <GameContext.Provider value={{ score, setScore, totalScore, calculateScore, setTotalScore, soundEnabled, setSoundEnabled }}>
+  return (
+    <GameContext.Provider value={{ score, setScore, soundEnabled, setSoundEnabled }}>
       {children}
     </GameContext.Provider>
   )
 }
 
-export {GameProvider, GameContext}
+export { GameProvider, GameContext }

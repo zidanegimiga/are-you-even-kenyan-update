9597a09316e442b0a5e71a3a7d7b3494df5513aa
zidanegimiga <zidanegimigatemp@gmail.com>
2023-09-25 16:55:29 +0300

    create keys for storage variables

--- global/constants/AsyncStorageKeys.js
+++ global/constants/AsyncStorageKeys.js
@@ -1,1 +1,2 @@
-export const QUESTION_PROGRESS_STORAGE_KEY =  "@questionProgress";
+export const QUESTION_PROGRESS_STORAGE_KEY = "@questionProgress";
+export const CORRECT_ANSWERS_KEY = "@correctAnswers";

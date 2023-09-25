9bd03543daef502680f69f6e94a44b31ffaf2f94
zidanegimiga <zidanegimigatemp@gmail.com>
2023-09-25 16:57:20 +0300

    reflect Feedback file name changes

--- navigations/OurRoads.js
+++ navigations/OurRoads.js
@@ -2,36 +2,36 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OurRoads from '../screens/OurRoads/OurRoads';
 import { Congratulations } from '../screens/OurRoads/Congratulations';
-import Somea from '../screens/OurRoads/Congratulationss';
-import BestPeople from '../screens/OurRoads/BestPeople';
+import Somea from '../screens/OurRoads/Somea';
+import FeedbackScreen from '../screens/OurRoads/Feedback';
 
 const Stack = createNativeStackNavigator();
 
 function OurRoadsNavigator() {
-    return (
-      <Stack.Navigator screenOptions={{}} >
-        <Stack.Screen
-          name={"Our Roads - Home"}
-          options={{headerShown: false}}
-          component={OurRoads}
-        />
-        <Stack.Screen
-          name={"Our Roads - Congratulations"}
-          options={{headerShown: false}}
-          component={Congratulations}
-        />
-        <Stack.Screen
-          name={"Somea"}
-          options={{headerShown: false}}
-          component={Somea}
-        />
-        <Stack.Screen
-          name={"Best People"}
-          options={{headerShown: false}}
-          component={BestPeople}
-        />
-      </Stack.Navigator>
-    );
-  }
-  
-  export default OurRoadsNavigator;
+  return (
+    <Stack.Navigator screenOptions={{}} >
+      <Stack.Screen
+        name={"Our Roads - Home"}
+        options={{ headerShown: false }}
+        component={OurRoads}
+      />
+      <Stack.Screen
+        name={"Our Roads - Congratulations"}
+        options={{ headerShown: false }}
+        component={Congratulations}
+      />
+      <Stack.Screen
+        name={"Somea"}
+        options={{ headerShown: false }}
+        component={Somea}
+      />
+      <Stack.Screen
+        name={"Best People"}
+        options={{ headerShown: false }}
+        component={FeedbackScreen}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default OurRoadsNavigator;
